5c534779698730bd2382e057d33e09318317148d
olehbilykh <olehbilykh@gmail.com>
2022-02-10 14:59:32 +0200

    working on

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const http = require("http");
-const mysql = require('mysql');
+const http = require('http')
+// const {readFileSync} = require('index.html')
 
+// const homePage = readFileSync('index.html')
 
-http.createServer(function (request, response) {
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/html'});
+const server = http.createServer((request, response) =>{
+    const url = request.url;
 
-    // Send the response body as "Hello World"
-    response.end('Hello World');
-}).listen(8081);
+    if(url === '/'){
+        response.writeHead(200,{'content-type' : 'text/plain'})
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+        response.write('home page');
+        response.end();
+    }else if(url === '/about'){
+        response.writeHead(200,{'content-type' : 'text/html'})
+
+        response.write('<h1>about page</h1>');
+        response.end();
+    }else if(url === '/hello'){
+        response.writeHead(200,{'content-type' : 'text/json'})
+
+        const responseData = "Hello World";
+        const jsonContent = JSON.stringify(responseData);
+        response.write(jsonContent);
+        response.end();
+    }else{
+        response.writeHead(404, {'content-type' : 'text/html'})
+        response.write('page doesnt exist')
+        response.end()
+    }
+}).listen(5000);
--- express/app.js
+++ express/app.js
@@ -1,20 +1,27 @@
 const http = require('http')
-const {readFileSync} = require('index.html')
+// const {readFileSync} = require('index.html')
 
-const homePage = readFileSync('./index.html')
+// const homePage = readFileSync('index.html')
 
 const server = http.createServer((request, response) =>{
     const url = request.url;
 
     if(url === '/'){
-        response.writeHead(200,{'content-type' : 'text/html'})
+        response.writeHead(200,{'content-type' : 'text/plain'})
 
-        response.write(homePage);
+        response.write('home page');
         response.end();
     }else if(url === '/about'){
         response.writeHead(200,{'content-type' : 'text/html'})
 
         response.write('<h1>about page</h1>');
+        response.end();
+    }else if(url === '/hello'){
+        response.writeHead(200,{'content-type' : 'text/json'})
+
+        const responseData = "Hello World";
+        const jsonContent = JSON.stringify(responseData);
+        response.write(jsonContent);
         response.end();
     }else{
         response.writeHead(404, {'content-type' : 'text/html'})
