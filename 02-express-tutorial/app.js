9f30b947529b0092c92c46cc9144cfc0b0003bf1
olehbilykh <olehbilykh@gmail.com>
2022-02-13 00:22:08 +0200

    dunno what to write here

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+var express = require("express");
+var app = express();
+app.get('/hello',function(req,res){
+    var myJson = {};
+    myJson.msg = "Hello World!";
+    res.json(myJson);
+});
+app.listen(8080);
