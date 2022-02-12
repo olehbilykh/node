cbd83ec01fb6b158c9faa003c7a77113b411cbb0
olehbilykh <olehbilykh@gmail.com>
2022-02-13 00:24:24 +0200

    dunno what to write here

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,26 @@
-var express = require("express");
-var app = express();
-app.get('/hello',function(req,res){
-    var myJson = {};
-    myJson.msg = "Hello World!";
-    res.json(myJson);
-});
-app.listen(8080);
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    // console.log(req.method)
+    const url = req.url
+    // home page
+    if (url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>home page</h1>')
+        res.end()
+    }
+    // about page
+    else if (url === '/about') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>about page</h1>')
+        res.end()
+    }
+    // 404
+    else {
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.write('<h1>page not found</h1>')
+        res.end()
+    }
+})
+
+server.listen(5000)
--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-const http = require('http')
-// const {readFileSync} = require('index.html')
-
-// const homePage = readFileSync('index.html')
-
-const server = http.createServer((request, response) =>{
-    const url = request.url;
-
-    if(url === '/'){
-        response.writeHead(200,{'content-type' : 'text/plain'})
-
-        response.write('home page');
-        response.end();
-    }else if(url === '/about'){
-        response.writeHead(200,{'content-type' : 'text/html'})
-
-        response.write('<h1>about page</h1>');
-        response.end();
-    }else if(url === '/hello'){
-        response.writeHead(200,{'content-type' : 'text/json'})
-
-        const responseData = "Hello World";
-        const jsonContent = JSON.stringify(responseData);
-        response.write(jsonContent);
-        response.end();
-    }else{
-        response.writeHead(404, {'content-type' : 'text/html'})
-        response.write('page doesnt exist')
-        response.end()
-    }
-}).listen(5000);
+var express = require("express");
+var app = express();
+app.get('/hello',function(req,res){
+    var myJson = {};
+    myJson.msg = "Hello World!";
+    res.json(myJson);
+});
+app.listen(8080);
